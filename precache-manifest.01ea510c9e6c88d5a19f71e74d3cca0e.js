2bfeda6539bcbe3fa60a919fb905a38e40c267a9
charanya06 <charanya1784@gmail.com>
2023-09-21 18:23:54 +0530

    Updates

---
+++ precache-manifest.01ea510c9e6c88d5a19f71e74d3cca0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99321e3de09ff172e30d5359128dbe9f",
+    "url": "/Delici_restaurant_web_FE/index.html"
+  },
+  {
+    "revision": "da803e1d968258b0c204",
+    "url": "/Delici_restaurant_web_FE/static/css/2.5711c12b.chunk.css"
+  },
+  {
+    "revision": "342719378f7ad14b4ac7",
+    "url": "/Delici_restaurant_web_FE/static/css/main.fd371dff.chunk.css"
+  },
+  {
+    "revision": "da803e1d968258b0c204",
+    "url": "/Delici_restaurant_web_FE/static/js/2.dc9a7242.chunk.js"
+  },
+  {
+    "revision": "2d68ee0d980bfcc9117aaa1f4b99eb3b",
+    "url": "/Delici_restaurant_web_FE/static/js/2.dc9a7242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342719378f7ad14b4ac7",
+    "url": "/Delici_restaurant_web_FE/static/js/main.1443d799.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Delici_restaurant_web_FE/static/js/main.1443d799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0945b7092af3c84ca4b2",
+    "url": "/Delici_restaurant_web_FE/static/js/runtime-main.9c8dbd28.js"
+  }
+]);
